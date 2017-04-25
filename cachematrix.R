cfaea33c30f196154fead767a6d912349b9fbaa1
Angel Ruiz Alonso <angel.alonso.contractor@bbva.com>
2017-04-25 17:47:08 -0500

    More changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,17 @@
-## Put comments here that give an overall description of what your
+## Function that creates the methods setMatrix, setInverse, getInverse
 ## functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-    inv <- NULL
+    inv <- NULL #variable to store the inverse of the matrix
     setMatrix <- function(y) {
         x <<- y
         inv <<- NULL
     }
-    getMatrix <- function() x
-    setInverse <- function(inverse) inv <<- inverse
-    getInverse <- function() inv
+    getMatrix <- function() x #Returns the matrix
+    setInverse <- function(inverse) inv <<- inverse  #Function that sets the inverse of a matrix
+    getInverse <- function() inv  #Function that returns the inverse of a matrix
     list(setMatrix = setMatrix,
          getMatrix = getMatrix,
          setInverse = setInverse,
@@ -19,12 +19,13 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Function that evaluates in first place is the invert matrix has been previously calculated
+## if so it returns it value, otherwise the invert matrix is calculated
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     inv <- x$getInverse()
-    if (!is.null(inv)) {
+    if (!is.null(inv)) { #Check is the inv variable has value if so returns it
         message("Already have values cached")
         return(inv)
     }
